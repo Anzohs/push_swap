a281320564bdce14f1c40e431910e6b9640b1e11
hladeiro <hladeiro@kali.kali>
2024-09-12 16:16:51 -0400

    adding checkers on the main function

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hladeiro <hladeiro@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 09:58:13 by hladeiro          #+#    #+#             */
-/*   Updated: 2024/09/12 15:36:52 by hladeiro         ###   ########.fr       */
+/*   Updated: 2024/09/12 16:14:10 by hladeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,28 +53,66 @@ void	ft_error(void)
 	exit(1);
 }
 
+void split_argv(char **argv, t_stack **stack_a)
+{
+	char **split;
+	int i;
+	int j;
+
+	i = -1;
+	split = ft_split(argv[1], ' ');
+	if (!split)
+	{
+		ft_putstr_fd("split of the string gonne wrong\n", 2);
+		return ;
+	}
+	while (split[++i] != NULL)
+	{
+		j = 0;
+		while (split[i][j])
+		{
+			if (!ft_isdigit(split[i][j]))
+				ft_error();
+			j++;
+		}
+		add_node(stack_a, ft_atoi(split[i]));
+		free(split[i]);
+	}
+	free(split);
+}
+
+void add_argv(int argc, char **argv, t_stack **stack_a)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (++i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (!ft_isdigit(argv[i][j]))
+				ft_error();
+			j++;
+		}
+		add_node(stack_a, ft_atoi(argv[i]));
+	}
+	if (!*stack_a)
+		ft_error();
+}
+
 int	main(int argc, char **argv)
 {
 	t_stack	*stack_a;
-	char	**split;
-	int	i;
 
 	stack_a = NULL;
 	if (argc < 2)
 		return (0);
 	if (argc == 2)
-		split = ft_split(argv[1], ' ');
-	i = -1;
-	while (split[++i] != NULL)
-	{
-		add_node(&stack_a, ft_atoi(split[i]));
-		free(split[i]);
-	}
-	if (!stack_a)
-	{
-		ft_error();
-		return (0);
-	}
+		split_argv(argv, &stack_a);
+	else
+		add_argv(argc, argv, &stack_a);
 	while (stack_a)
 	{
 		t_stack *tmp = stack_a;
@@ -82,6 +120,5 @@ int	main(int argc, char **argv)
 		stack_a = stack_a->next;
 		free(tmp);
 	}
-	free(split);
 	return (0);
 }
